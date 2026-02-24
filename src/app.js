f8bd2494c15c744d0a8d407eda6926a7a57504f9
Tasfia Sultana <tasfiaa024@gmail.com>
2026-02-25 03:21:31 +0600

    fix: add Netlify frontend URL to CORS allowed origins

--- src/app.js
+++ src/app.js
@@ -9,13 +9,15 @@ import { errorHandler } from "./middlewares/error.middleware.js";
 const app = express();
 
 app.use(helmet());
-app.use(cors({ origin: "http://localhost:5173" }));
-// app.use(cors({ 
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization']
-// }));
+app.use(cors({ 
+    origin: [
+        "http://localhost:5173",
+        "https://soft-meerkat-ac0795.netlify.app"
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
